527e2e0a02c64924b1d09b6e58b3e06245e58945
Hyung Kim <hjkmines@gmail.com>
2020-06-25 22:50:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+
+class School 
+  attr_accessor :name, :roster 
+  
+  
+  def initialize(name)
+    name = name 
+  end
+  
+  def roster 
+    roster = {}
+  end 
+  
+  def add_student(student, grade)
+    roster[grade] = []
+    roster[grade] << student 
+  end 
+  
+  def grade(which_grade)
+    roster[which_grade]
+  end 
+  
+  def sort 
+    roster.sort 
+  end 
+  
+end 
